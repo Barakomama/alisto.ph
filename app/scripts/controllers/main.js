12c30150ce81d2754f45a55e6202e2d066eb2745
Joey Hipolito <hi@joeyhipolito.com>
2014-05-10 14:50:21 +0800

    port to a facebook app, make it work

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('alistophApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, simpleLogin) {
+    
+    $scope.login = function (provider) {
+      simpleLogin.login(provider, function(err) {
+        $scope.err = err? err + '' : null;
+      });
+    }
+
   });
